7042ee6e9ee356e6d1fd7ab83999870a03f3287e
matt y <matthew.young@vtsd.com>
2025-12-04 13:55:09 -0500

    function_example.py

--- 1.2.1/turtle_game.py
+++ 1.2.1/turtle_game.py
@@ -1,10 +1,10 @@
-# a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
 import random as rand
 
 #-----game configuration----
 spot_color = "blue"
+score = 0
 
 #-----initialize turtle-----
 painter = trtl.Turtle()
@@ -16,7 +16,30 @@ painter.penup()
 #-----game functions--------
 #get a score boost, move the turtle randomly
 def spot_clicked(x, y):
-    painter.teleport(rand.randint(-320, 320), rand.randint(-320, 320))
+    change_position()
+    update_score()
+
+def change_position():
+    painter.teleport(rand.randint(-360, 360), rand.randint(-360, 300))
+
+def update_score():
+    #include global score
+    global score
+    #increment the score by 1
+    score += 1
+    #print the score
+    print(score)
+
+import turtle as trtl
+score_writer = trtl.Turtle()
+score_writer.teleport(0, 310)
+score_writer.width(5)
+score_writer.forward(60)
+score_writer.left(90)
+score_writer.forward(60)
+score_writer.left(90)
+score_writer.forward(120)
+score_wr
 
 #-----events----------------
 painter.onclick(spot_clicked)
