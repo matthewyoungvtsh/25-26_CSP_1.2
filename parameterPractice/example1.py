b795451fc5c9ebe2095740b6f9abb12390dea4f5
matt y <matthew.young@vtsd.com>
2025-12-17 13:08:56 -0500

    function_example.py

---
+++ parameterPractice/example1.py
@@ -0,0 +1,13 @@
+#method to add numbers
+def add_numbers(one, two):
+    sum = one + two
+    return sum
+
+def combine_numbers(num1, num2, num3):
+    sum2 = (num1 + num2) * num3
+    return sum2
+
+num1 = 1
+num2 = 2
+num3 = 4
+print(combine_numbers(num1, num2, num3))
