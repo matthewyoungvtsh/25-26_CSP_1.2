a664f1b97c95885f1cc8b6c28ff123ad1871c3b4
matt y <matthew.young@vtsd.com>
2026-01-06 11:46:13 -0500

    Apple Game Update

--- 1.2.3/main.py
+++ 1.2.3/main.py
@@ -5,28 +5,39 @@ import turtle as trtl
 apple_image = "apple.gif" # Store the file name of your shape
 
 wn = trtl.Screen()
+wn.bgpic("background.gif")
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 
 apple = trtl.Turtle()
-letter = trtl.Turtle()
+wn.tracer(False)
+apple.penup()
+drawn_letter = trtl.Turtle()
+
+alphabet.list("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-  draw_letter()
+  draw_letter("a", active_apple)
+  wn.tracer(True)
   wn.update()
 
-def draw_letter():
-    letter.teleport(0, 400)
-    letter.shape("a")
+
+def draw_letter(letter, active_apple):
+    drawn_letter.teleport(0, 400)
+    drawn_letter.write("a", font=("Arial", 60, "bold"))
 
 def drop_apple():
+    wn.tracer(True)
     apple.goto(apple.xcor(), ground_height)
+    apple.hideturtle()
+    drawn_letter.clear()
+    wn.tracer(False)
 
 #-----function calls-----
-draw_apple(apple)
+draw_apple(active_apple)
 wn.onkeypress(drop_apple, "a")
 
 wn.mainloop()
